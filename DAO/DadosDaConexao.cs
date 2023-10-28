ae425f6ac9df19062fdb546ec543f33449a7c6a9
Diogo Souza <50971306+dgsouzav@users.noreply.github.com>
2023-10-28 15:19:10 -0300

    Add files via upload

---
+++ DAO/DadosDaConexao.cs
@@ -0,0 +1,13 @@
+﻿namespace DAL
+{
+    public class DadosDaConexao
+    {
+        public static String StringDeConexao
+        {
+            get
+            {
+                return "Data Source=DGSOUZAV-PC; Integrated Security = True";
+            }
+        }
+    }
+}
