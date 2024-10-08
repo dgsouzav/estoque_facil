2db65bb13bce2ddf189c992ec99551e0e1e8e934
dgsouzav <diogouba11@gmail.com>
2024-10-08 20:44:00 -0300

    123

--- DAO/DALRelatorioVenda.cs
+++ DAO/DALRelatorioVenda.cs
@@ -31,6 +31,22 @@ namespace DAL
             public int TotalDeVendas { get { return _total_de_vendas; } }
         }
 
+        public class VendaGrafico
+        {
+            string _data;
+            int _numero_de_vendas;
+            int _total_de_vendas;
+            public VendaGrafico(string data, int numero_de_vendas, int total_de_vendas)
+            {
+                this._data = data;
+                this._numero_de_vendas = numero_de_vendas;
+                this._total_de_vendas = total_de_vendas;
+            }
+            public string Data { get { return _data; } }
+            public int NumeroDeVendas { get { return _numero_de_vendas; } }
+            public int TotalDeVendas { get { return _total_de_vendas; } }
+        }
+
         public List<VendaRelatorio>? ObterLinhasVenda(string nomeTabela, DateTime dataInicial, DateTime dataFinal)
         {
             List<VendaRelatorio> vendaColunas = new List<VendaRelatorio>();
@@ -60,6 +76,36 @@ namespace DAL
                 this.conexao.Desconectar();
             }
             return vendaColunas;
+        }
+        public List<VendaGrafico>? ObterGrafico(string nomeTabela)
+        {
+            List<VendaGrafico> graficoColunas = new List<VendaGrafico>();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand("SELECT data, numero_de_vendas, total_de_vendas FROM vwrelatoriovendas;");
+                cmd.Connection = conexao.ObjetoConexao;
+                this.conexao.Conectar();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr != null) 
+                {
+                    while (dr.Read())
+                    {
+                        VendaGrafico grafico = new VendaGrafico(Convert.ToString(dr["data"]), Convert.ToInt32(dr["numero_de_vendas"]),
+                            Convert.ToInt32(dr["total_de_vendas"]));
+                        graficoColunas.Add(grafico);
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                this.conexao.Desconectar();
+            }
+            return graficoColunas;
         }
         public double ObterTotalVendas(DateTime dataInicial, DateTime dataFinal)
         {
